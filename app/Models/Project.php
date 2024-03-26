3a7c936d3042d815a328d64184547f1713ad6a76
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-03-26 12:44:54 +0100

    generate slug

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,7 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        'id',
+        'title',
+        'img',
+        'description',
+        'software',
+        'slug'
+
+    ];
+
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
 }
