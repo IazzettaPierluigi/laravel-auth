1f7402c16eafbff5dbba24dce425e24095c7391d
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-03-27 17:39:30 +0100

    projects table in dashboard index

--- resources/views/pages/projects/index.blade.php
+++ resources/views/pages/projects/index.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+<main class="container">
+
     <h1>lista dei projects</h1>
+
+    <div
+        class="table-responsive"
+    >
+        <table
+            class="table"
+        >
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">TITLE</th>
+                    <th scope="col">IMAGE</th>
+                    <th scope="col">SOFTWARE</th>
+                    <th scope="col">SLUG</th>
+                </tr>
+            </thead>
+            <tbody>@foreach ($projects as $item)
+                
+                <tr class="">
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->title }}</td>
+                    <td>{{ $item->img }}</td>
+                    <td>{{ $item->description }}</td>
+                    <td>{{ $item->software }}</td>
+                </tr>
+            @endforeach
+               
+            </tbody>
+        </table>
+    </div>
+    
+</main>
 @endsection
